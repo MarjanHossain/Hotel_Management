67106132a2e0ee91ad02934a14acffc1b192ddc1
MarjanHossain <98733927+MarjanHossain@users.noreply.github.com>
2024-03-17 22:37:23 +0600

    project start

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+const App = () => {
+  return (
+    <main>
+      <h1 className="text-3xl font-bold underline text-purple-800">
+        Hello world!
+      </h1>    </main>
+  );
+};
+
+export default App;
+
+// 
+// git branch - M main
+// git remote add origin https://github.com/MarjanHossain/Hotel_Management.git
+// git push - u origin main
